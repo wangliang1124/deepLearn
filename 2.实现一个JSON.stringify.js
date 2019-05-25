2dc65e1d6cb96932d7672262c3575c0e274bb941
Wang Liang <wangliang@souche.com>
2019-05-25 18:43:08 +0800

    实现 JSON.stringify 和 JSON.parse

--- 2.实现一个JSON.stringify.js
+++ 2.实现一个JSON.stringify.js
@@ -1,29 +1,70 @@
- /* 2. 实现一个JSON.stringify */
- function stringify(obj) {
+// 2. 实现一个JSON.stringify
+function stringify(obj) {
     const type = typeof obj;
     if (/undefined|function|symbol/.test(type)) {
-        return undefined
+        return undefined;
     }
-    if (/string|number|boolean/.test(type) || obj === null) {
-        return `"${obj}"`
+    if (/number|boolean/.test(type) || obj === null) {
+        return String(obj);
     }
-    const arr = Array.isArray(obj) ? obj : Object.values(obj)
-    let json = []
-    for (let i = 0, len = arr.length; i < len; i++) {
-        let v = arr[i]
-        let type = typeof v;
-        if (/undefined|function|symbol/.test(type) || obj === null) {
-            json.push(null)
-        } else {
-            json.push(v)
-        }
+    if (type === "string") {
+        return `"${obj}"`;
     }
-    console.log(json)
-
-    return '"[' + String(json) + ']"';
+    let result = "";
+    switch (Object.prototype.toString.call(obj)) {
+        case "[object Array]":
+            result += "[";
+            for (let v of obj) {
+                const val = stringify(v);
+                result += `${val === undefined ? null : val},`;
+            }
+            // 去掉最后一个逗号
+            if (result !== "[") {
+                result = result.slice(0, -1);
+            }
+            return (result += "]");
+        case "[object Object]":
+            result += "{";
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    const val = stringify(obj[key]);
+                    if (val !== undefined) {
+                        result += `"${key}": ${val},`;
+                    }
+                }
+            }
+            if (result !== "{") {
+                result = result.slice(0, -1);
+            }
+            return (result += "}");
+        case "[object Date]":
+            return `"${obj.toJSON ? obj.toJSON() : obj.toString()}"`;
+        default:
+            return "{}";
+    }
 }
 
-console.log(stringify(Object))
-console.log(stringify(null))
-console.log(stringify(2))
-console.log(stringify(false)
+console.log(stringify(Object));
+console.log(stringify(null));
+console.log(stringify(2));
+console.log(stringify(false));
+console.log(stringify("false"));
+console.log(stringify({ a: 1, b: 2 }));
+console.log(
+    stringify([
+        false,
+        Symbol(),
+        Symbol,
+        new Date(),
+        new Error("测试"),
+        undefined,
+        "测试",
+        null,
+        /./,
+        { a: 1, b: 2, c: null },
+        [1, undefined],
+        Number(1),
+    ]),
+);
+
+ // JSON.stringify 和 JSON.parse 的实现 https://www.jianshu.com/p/f1c8bcd16f71
