6e3dd89cbd1a78c051185aa4a7ece914f4cb93be
Wang Liang <wangliang@souche.com>
2019-07-17 15:37:12 +0800

    update

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,45 +1,44 @@
-// 观察者模式： 请实现一个 EventEmitter 类（） 继承自此类的对象都会拥有两个方法 on, off, once 和 trigger
-var EventEmitter = function () {
-    if (!this instanceof Event) {
-        return new Event()
-    }
-    this._callbacks = {}
-}
+/* 实现一个 EventEmitter(观察者模式) 类,拥有 on, off, once 和 emit 方法 */
+const EventEmitter = function() {
+    this.eventQueue = {};
+};
+
 EventEmitter.prototype = {
     constructor: EventEmitter,
-    on: function (type, handler) {
-        this._callbacks = this._callbacks || {}
-        this._callbacks[type] = this._callbacks[type] || []
-        this._callbacks[type].push(handler)
-        return this
+
+    on: function(type, handler) {
+        this.eventQueue[type] = this.eventQueue[type] || [];
+        this.eventQueue[type].push(handler);
+        return this;
     },
-    off: function (type, handler) {
-        var list = this._callbacks[type]
-        if (list.length) {
-            for (var i = list.length - 1; i >= 0; i--) {
-                if (list[i] === handler) {
-                    list.splice(i, 1)
-                }
+
+    off: function(type, handler) {
+        const eventHandlers = this.eventQueue[type];
+        for (let i = eventHandlers.length - 1; i >= 0; i--) {
+            if (eventHandlers[i] === handler) {
+                eventHandlers.splice(i, 1);
             }
         }
-        return this
+
+        return this;
     },
-    emit: function (type, data) {
-        var list = this._callbacks[type]
-        var len = list.length
-        if (len) {
-            for (var i = 0; i < len; i++) {
-                list[i].call(this, data)
-            }
+
+    emit: function(type, data) {
+        const eventHandlers = this.eventQueue[type];
+        for (let i = 0; i < eventHandlers.length; i++) {
+            const handler = eventHandlers[i];
+            handler && handler(data);
         }
+        return this;
     },
-    once: function (type, handler) {
-        var self = this
-        var wrapper = function () {
-            handler.apply(self, arguments)
-            self.off(type, wrapper)
-        }
-        this.on(type, wrapper)
-        return this
-    }
-}
+
+    once: function(type, handler) {
+        this.on(type, data => {
+            handler && handler(data);
+            this.off(type, wrapper);
+        });
+        return this;
+    },
+};
+
+export default new EventEmitter();
