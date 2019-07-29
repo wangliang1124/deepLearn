140fe68e2c1e1c59453e3513bc9da6dde24d5aef
Wang Liang <wangliang@souche.com>
2019-07-29 18:48:24 +0800

    Update

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,7 +1,7 @@
 /* 实现一个 EventEmitter(观察者模式) 类,拥有 on, off, once 和 emit 方法 */
-const EventEmitter = function() {
+function EventEmitter() {
     this.eventQueue = {};
-};
+}
 
 EventEmitter.prototype = {
     constructor: EventEmitter,
@@ -13,33 +13,33 @@ EventEmitter.prototype = {
     },
 
     off: function(type, handler) {
-        const handlers = this.eventQueue[type];
-        for (let i = handlers.length - 1; i >= 0; i--) {
-            if (handlers[i] === handler) {
-                handlers.splice(i, 1);
-            }
-        }
-
-        return this;
-    },
-
-    emit: function(type, data) {
-        const handlers = this.eventQueue[type];
-        if (!handlers || handlers.length === 0) return;
+        let handlers = this.eventQueue[type];
         for (let i = 0; i < handlers.length; i++) {
-            const handler = handlers[i];
-            handler && handler(data);
+            if (handler === handlers[i]) {
+                this.eventQueue[type].splice(i, 1);
+            }
         }
         return this;
     },
 
     once: function(type, handler) {
-        this.on(type, data => {
-            handler && handler(data);
-            this.off(type, wrapper);
-        });
+        const self = this;
+        const wrapper = function(...args) {
+            handler && handler.call(null, ...args);
+            self.off(type, wrapper);
+        };
+        this.on(type, wrapper);
+        return this;
+    },
+
+    emit: function(type, ...args) {
+        let handlers = this.eventQueue[type];
+        if (!handlers || handlers.length === 0) return;
+        for (let i = 0; i < handlers.length; i++) {
+            let handler = handlers[i];
+            handler && handler.call(null, ...args);
+        }
         return this;
     },
 };
-
 export default new EventEmitter();
